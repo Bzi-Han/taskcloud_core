992db84eca097062679191ac51d524a503241663
Bzi_Han <rosebzi@foxmail.com>
2022-08-05 15:29:01 +0800

    Add gbk/utf8 convert, urlEn/Decode, rsaEn/Decrypt

--- includes/ModuleCrypto.h
+++ includes/ModuleCrypto.h
@@ -14,18 +14,54 @@
 #include <cryptopp/modes.h>
 #include <cryptopp/aes.h>
 #include <cryptopp/des.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/cpu.h>
+#include <cryptopp/randpool.h>
+#include <cryptopp/rdrand.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+
+#include <windows.h>
+
+#define PLATFORM_WINDOWS
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+
+#include <iconv.h>
+
+#define PLATFORM_LINUX
+
+#endif
 
 #include <string>
+#include <string_view>
+#include <sstream>
 
 namespace ModuleCrypto
 {
     namespace Detail
     {
+        bool isBase64String(const std::string &data);
+
+        std::string rsaBase64KeyNormalize(const std::string_view &data);
+
         template <typename algorithm_t>
         std::string messageDigest(const std::string &data);
 
         template <typename algorithm_t>
         std::string symmetric(const std::string &data, const std::string &key, const std::string &iv, const std::string &padding);
+
+        template <bool is_encrypt, typename algorithm_t, typename decoder_t, typename encoder_t = CryptoPP::Base64Encoder>
+        std::string asymmetric(const std::string &data, const std::string &key);
+    }
+
+    namespace Bindings
+    {
+        luabridge::LuaRef luaRsaGenerateKeyPair(lua_State *luaState);
+
+        pybind11::dict pyRsaGenerateKeyPair(pybind11::args args);
+
+        JSValue jsRsaGenerateKeyPair(quickjs::args args);
     }
 
     void bind(lua_State *luaState);
@@ -33,6 +69,12 @@ namespace ModuleCrypto
     void bind(PyObject *mainModule);
 
     void bind(JSContext *context);
+
+    std::string gbkToUTF8(const std::string &gbk);
+    std::string utf8ToGBK(const std::string &utf8);
+
+    std::string urlEncode(const std::string &data);
+    std::string urlDecode(const std::string &data);
 
     std::string base64Encode(const std::string &data);
     std::string base64Decode(const std::string &data);
@@ -47,6 +89,21 @@ namespace ModuleCrypto
 
     std::string desEncrypt(const std::string &data, const std::string &key, const std::string &iv, const std::string &mode, const std::string &padding);
     std::string desDecrypt(const std::string &data, const std::string &key, const std::string &iv, const std::string &mode, const std::string &padding);
+
+    /**
+     * @name rsaGenerateKeyPair
+     * @brief generate a key pair
+     *
+     * @param keySize key size, default is 1024.
+     * @param hex whether to use hexadecimal encoding, the default is true. otherwise, use base64 encoding.
+     * @param seed user input seed, default is empty.
+     *
+     * @return KeyPair<PublicKey，PrivateKey>
+     */
+    std::pair<std::string, std::string> rsaGenerateKeyPair(size_t keySize = 1024, bool hex = true, std::string seed = "");
+
+    std::string rsaEncrypt(const std::string &data, const std::string &publicKey, bool oaep = true);
+    std::string rsaDecrypt(const std::string &data, const std::string &privateKey, bool oaep = true);
 }
 
 #endif // !MODULE_CRYPTO_H
--- src/common/ModuleCrypto.cc
+++ src/common/ModuleCrypto.cc
@@ -2,6 +2,41 @@
 
 namespace ModuleCrypto::Detail
 {
+    bool isBase64String(const std::string &data)
+    {
+        return std::string::npos != data.find('=') || std::string::npos != data.find('+') || std::string::npos != data.find('/');
+    };
+
+    std::string rsaBase64KeyNormalize(const std::string_view &data)
+    {
+        std::stringstream result;
+        std::string_view key = data;
+
+        auto startPos = data.find("KEY-----");
+        auto endPos = startPos;
+
+        if (std::string::npos != startPos)
+        {
+            startPos += 8;
+            endPos = data.rfind("-----END");
+
+            if (std::string::npos == endPos)
+                throw std::runtime_error("invalid rsa key");
+
+            key = data.substr(startPos, endPos - startPos);
+        }
+
+        for (auto c : key)
+        {
+            if ('\n' == c || '\r' == c || ' ' == c || '\t' == c)
+                continue;
+
+            result << c;
+        }
+
+        return result.str();
+    }
+
     template <typename algorithm_t>
     std::string messageDigest(const std::string &data)
     {
@@ -55,6 +90,129 @@ namespace ModuleCrypto::Detail
 
         return result;
     }
+
+    template <bool is_encrypt, typename algorithm_t, typename decoder_t, typename encoder_t>
+    std::string asymmetric(const std::string &data, const std::string &key)
+    {
+        std::string result;
+        CryptoPP::RandomPool randomPool;
+
+        if (CryptoPP::HasRDSEED())
+        {
+            CryptoPP::byte seed[8]{};
+            CryptoPP::RDSEED rdseed;
+
+            rdseed.GenerateBlock(seed, 8);
+            randomPool.IncorporateEntropy(seed, sizeof(seed));
+        }
+
+        CryptoPP::StringSource rsaKey(key, true, new decoder_t);
+        if constexpr (is_encrypt)
+        {
+            algorithm_t encryptor(rsaKey);
+
+            CryptoPP::StringSource(
+                data,
+                true,
+                new CryptoPP::PK_EncryptorFilter(
+                    randomPool,
+                    encryptor,
+                    new encoder_t(
+                        new CryptoPP::StringSink(result))));
+        }
+        else
+        {
+            algorithm_t decryptor(rsaKey);
+
+            CryptoPP::StringSource(
+                data,
+                true,
+                new decoder_t(
+                    new CryptoPP::PK_DecryptorFilter(
+                        randomPool,
+                        decryptor,
+                        new CryptoPP::StringSink(result))));
+        }
+
+        return result;
+    }
+}
+
+namespace ModuleCrypto::Bindings
+{
+    luabridge::LuaRef luaRsaGenerateKeyPair(lua_State *luaState)
+    {
+        auto paramsCount = lua_gettop(luaState);
+
+        size_t keySize = 1024;
+        bool hex = true;
+        std::string seed;
+
+        if (1 <= paramsCount)
+        {
+            keySize = luaL_checkinteger(luaState, 1);
+            if (2 <= paramsCount)
+                hex = lua_toboolean(luaState, 2);
+            if (3 <= paramsCount)
+                seed = luaL_checkstring(luaState, 3);
+        }
+
+        auto keyPair = rsaGenerateKeyPair(keySize, hex, seed);
+        auto result = luabridge::LuaRef::newTable(luaState);
+
+        result["publicKey"] = keyPair.first;
+        result["privateKey"] = keyPair.second;
+
+        return result;
+    }
+
+    pybind11::dict pyRsaGenerateKeyPair(pybind11::args args)
+    {
+        size_t keySize = 1024;
+        bool hex = true;
+        std::string seed;
+
+        if (1 <= args.size())
+        {
+            keySize = args[0].cast<size_t>();
+            if (2 <= args.size())
+                hex = args[1].cast<bool>();
+            if (3 <= args.size())
+                seed = args[2].cast<std::string>();
+        }
+
+        auto keyPair = rsaGenerateKeyPair(keySize, hex, seed);
+        pybind11::dict result;
+
+        result["publicKey"] = keyPair.first;
+        result["privateKey"] = keyPair.second;
+
+        return result;
+    }
+
+    JSValue jsRsaGenerateKeyPair(quickjs::args args)
+    {
+        size_t keySize = 1024;
+        bool hex = true;
+        std::string seed;
+
+        if (1 <= args.size())
+        {
+            keySize = args[0].cast<size_t>();
+            if (2 <= args.size())
+                hex = args[1].cast<bool>();
+            if (3 <= args.size())
+                seed = args[2].cast<std::string>();
+        }
+
+        auto keyPair = rsaGenerateKeyPair(keySize, hex, seed);
+        quickjs::object result(args);
+
+        result.setProperty("publicKey", keyPair.first);
+        result.setProperty("privateKey", keyPair.second);
+
+        return result;
+    }
 }
 
 namespace ModuleCrypto
@@ -63,6 +221,10 @@ namespace ModuleCrypto
     {
         luabridge::getGlobalNamespace(luaState)
             .beginNamespace("crypto")
+            .addFunction("gbkToUTF8", &gbkToUTF8)
+            .addFunction("utf8ToGBK", &utf8ToGBK)
+            .addFunction("urlEncode", &urlEncode)
+            .addFunction("urlDecode", &urlDecode)
             .addFunction("base64Encode", &base64Encode)
             .addFunction("base64Decode", &base64Decode)
             .addFunction("md5", &md5)
@@ -73,6 +235,9 @@ namespace ModuleCrypto
             .addFunction("aesDecrypt", &aesDecrypt)
             .addFunction("desEncrypt", &desEncrypt)
             .addFunction("desDecrypt", &desDecrypt)
+            .addFunction("rsaGenerateKeyPair", &Bindings::luaRsaGenerateKeyPair)
+            .addFunction("rsaEncrypt", &rsaEncrypt)
+            .addFunction("rsaDecrypt", &rsaDecrypt)
             .endNamespace();
     }
 
@@ -81,6 +246,10 @@ namespace ModuleCrypto
         auto module = pybind11::cast<pybind11::module>(mainModule);
 
         auto cryptoModule = module.def_submodule("crypto");
+        cryptoModule.def("gbkToUTF8", &gbkToUTF8);
+        cryptoModule.def("utf8ToGBK", &utf8ToGBK);
+        cryptoModule.def("urlEncode", &urlEncode);
+        cryptoModule.def("urlDecode", &urlDecode);
         cryptoModule.def("base64Encode", &base64Encode);
         cryptoModule.def("base64Decode", &base64Decode);
         cryptoModule.def("md5", &md5);
@@ -91,12 +260,19 @@ namespace ModuleCrypto
         cryptoModule.def("aesDecrypt", &aesDecrypt);
         cryptoModule.def("desEncrypt", &desEncrypt);
         cryptoModule.def("desDecrypt", &desDecrypt);
+        cryptoModule.def("rsaGenerateKeyPair", &Bindings::pyRsaGenerateKeyPair);
+        cryptoModule.def("rsaEncrypt", &rsaEncrypt);
+        cryptoModule.def("rsaDecrypt", &rsaDecrypt);
     }
 
     void bind(JSContext *context)
     {
         auto cryptoModule = quickjs::object(context);
 
+        cryptoModule.addFunction<gbkToUTF8>("gbkToUTF8");
+        cryptoModule.addFunction<utf8ToGBK>("utf8ToGBK");
+        cryptoModule.addFunction<urlEncode>("urlEncode");
+        cryptoModule.addFunction<urlDecode>("urlDecode");
         cryptoModule.addFunction<base64Encode>("base64Encode");
         cryptoModule.addFunction<base64Decode>("base64Decode");
         cryptoModule.addFunction<md5>("md5");
@@ -107,8 +283,186 @@ namespace ModuleCrypto
         cryptoModule.addFunction<aesDecrypt>("aesDecrypt");
         cryptoModule.addFunction<desEncrypt>("desEncrypt");
         cryptoModule.addFunction<desDecrypt>("desDecrypt");
+        cryptoModule.addFunction<Bindings::jsRsaGenerateKeyPair>("rsaGenerateKeyPair");
+        cryptoModule.addFunction<rsaEncrypt>("rsaEncrypt");
+        cryptoModule.addFunction<rsaDecrypt>("rsaDecrypt");
 
         quickjs::object::getGlobal(context).addObject("crypto", cryptoModule);
+    }
+
+    std::string gbkToUTF8(const std::string &gbk)
+    {
+#if defined(PLATFORM_WINDOWS)
+
+        int convertLength = MultiByteToWideChar(CP_ACP, 0, gbk.data(), -1, nullptr, 0);
+        wchar_t *convertStringBuffer = new wchar_t[convertLength]{};
+        MultiByteToWideChar(CP_ACP, 0, gbk.data(), static_cast<int>(gbk.size()), convertStringBuffer, convertLength);
+
+        convertLength = WideCharToMultiByte(CP_UTF8, 0, convertStringBuffer, convertLength, nullptr, 0, nullptr, nullptr);
+        std::string result(convertLength, 0);
+        WideCharToMultiByte(CP_UTF8, 0, convertStringBuffer, -1, result.data(), convertLength, nullptr, nullptr);
+        delete[] convertStringBuffer;
+
+#elif defined(PLATFORM_LINUX)
+
+        std::string result;
+        iconv_t cd = iconv_open("UTF-8", "GBK");
+
+        if ((iconv_t)-1 == cd)
+            return {};
+
+        size_t inSize = gbk.size();
+        size_t outSize = 4 * inSize;
+        char *inBuffer = const_cast<char *>(gbk.data());
+        result.resize(outSize);
+        char *outBuffer = const_cast<char *>(result.data());
+
+        size_t status = iconv(cd, &inBuffer, &inSize, &outBuffer, &outSize);
+        if ((size_t)-1 == status)
+            return {};
+
+        result.resize(outSize);
+        iconv_close(cd);
+
+#endif
+
+        return result;
+    }
+
+    std::string utf8ToGBK(const std::string &utf8)
+    {
+#if defined(PLATFORM_WINDOWS)
+
+        int convertLength = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), -1, nullptr, 0);
+        wchar_t *convertStringBuffer = new wchar_t[convertLength]{};
+        MultiByteToWideChar(CP_UTF8, 0, utf8.data(), static_cast<int>(utf8.size()), convertStringBuffer, convertLength);
+
+        convertLength = WideCharToMultiByte(CP_ACP, 0, convertStringBuffer, convertLength, nullptr, 0, nullptr, nullptr);
+        std::string result(convertLength, 0);
+        WideCharToMultiByte(CP_ACP, 0, convertStringBuffer, -1, result.data(), convertLength, nullptr, nullptr);
+        delete[] convertStringBuffer;
+
+#elif defined(PLATFORM_LINUX)
+
+        std::string result;
+        iconv_t cd = iconv_open("GBK", "UTF-8");
+
+        if ((iconv_t)-1 == cd)
+            return {};
+
+        size_t inSize = utf8.size();
+        size_t outSize = 4 * inSize;
+        char *inBuffer = const_cast<char *>(utf8.data());
+        result.resize(outSize);
+        char *outBuffer = const_cast<char *>(result.data());
+
+        size_t status = iconv(cd, &inBuffer, &inSize, &outBuffer, &outSize);
+        if ((size_t)-1 == status)
+            return {};
+
+        result.resize(outSize);
+        iconv_close(cd);
+
+#endif
+
+        return result;
+    }
+
+    std::string urlEncode(const std::string &data)
+    {
+        std::stringstream result;
+
+        for (const auto c : data)
+        {
+            if (('0' <= c && '9' >= c) || ('a' <= c && 'z' >= c) || ('A' <= c && 'Z' >= c) || '-' == c || '_' == c || '.' == c || '~' == c)
+            {
+                result << c;
+
+                continue;
+            }
+
+            switch (c)
+            {
+            case ' ':
+                result << '+';
+
+                break;
+            default:
+            {
+                unsigned char l = c, h = c;
+                l = l >> 4;
+                h = h % 16;
+
+                result << '%'
+                       << (char)(l > 9 ? l + 55 : l + 48)
+                       << (char)(h > 9 ? h + 55 : h + 48);
+
+                break;
+            }
+            }
+        }
+
+        return result.str();
+    }
+
+    std::string urlDecode(const std::string &data)
+    {
+        std::stringstream result;
+
+        for (auto it = data.begin(); it != data.end(); it++)
+        {
+            if (('0' <= *it && '9' >= *it) || ('a' <= *it && 'z' >= *it) || ('A' <= *it && 'Z' >= *it) || '-' == *it || '_' == *it || '.' == *it || '~' == *it)
+            {
+                result << *it;
+
+                continue;
+            }
+
+            switch (*it)
+            {
+            case '+':
+                result << ' ';
+
+                break;
+            case '%':
+            {
+                unsigned char sum = 0;
+
+                for (size_t i = 0; i < 2; i++)
+                {
+                    ++it;
+
+                    if (*it >= '0' && *it <= '9')
+                    {
+                        if (0 == i)
+                            sum += (unsigned char)(*it - '0') * 16;
+                        else
+                            sum += (unsigned char)(*it - '0');
+                    }
+                    else if (*it >= 'A' && *it <= 'F')
+                    {
+                        if (0 == i)
+                            sum += ((unsigned char)(*it - 'A') + 10) * 16;
+                        else
+                            sum += (unsigned char)(*it - 'A') + 10;
+                    }
+                    else if (*it >= 'a' && *it <= 'f')
+                    {
+                        if (0 == i)
+                            sum += ((unsigned char)(*it - 'a') + 10) * 16;
+                        else
+                            sum += (unsigned char)(*it - 'a') + 10;
+                    }
+                }
+
+                result << sum;
+
+                break;
+            }
+            }
+        }
+
+        return result.str();
     }
 
     std::string base64Encode(const std::string &data)
@@ -235,5 +589,85 @@ namespace ModuleCrypto
             throw std::runtime_error("Unknown mode");
 
         return result;
+    }
+
+    std::pair<std::string, std::string> rsaGenerateKeyPair(size_t keySize, bool hex, std::string seed)
+    {
+        std::string publicKey, privateKey;
+        CryptoPP::RandomPool randomPool;
+
+        if (!seed.empty())
+            randomPool.IncorporateEntropy(reinterpret_cast<const CryptoPP::byte *>(seed.data()), seed.size());
+
+        if (hex)
+        {
+            CryptoPP::RSAES_OAEP_SHA_Decryptor decryptor(randomPool, keySize);
+            CryptoPP::HexEncoder privateEncoder(new CryptoPP::StringSink(privateKey));
+
+            decryptor.AccessMaterial().Save(privateEncoder);
+            privateEncoder.MessageEnd();
+
+            CryptoPP::RSAES_OAEP_SHA_Encryptor encryptor(decryptor);
+            CryptoPP::HexEncoder publicEncoder(new CryptoPP::StringSink(publicKey));
+
+            encryptor.AccessMaterial().Save(publicEncoder);
+            publicEncoder.MessageEnd();
+        }
+        else
+        {
+            CryptoPP::RSAES_OAEP_SHA_Decryptor decryptor(randomPool, keySize);
+            CryptoPP::Base64Encoder privateEncoder(new CryptoPP::StringSink(privateKey));
+
+            decryptor.AccessMaterial().Save(privateEncoder);
+            privateEncoder.MessageEnd();
+
+            CryptoPP::RSAES_OAEP_SHA_Encryptor encryptor(decryptor);
+            CryptoPP::Base64Encoder publicEncoder(new CryptoPP::StringSink(publicKey));
+
+            encryptor.AccessMaterial().Save(publicEncoder);
+            publicEncoder.MessageEnd();
+        }
+
+        return std::make_pair(publicKey, privateKey);
+    }
+
+    std::string rsaEncrypt(const std::string &publicKey, const std::string &data, bool oaep)
+    {
+        bool isBase64 = Detail::isBase64String(publicKey);
+
+        if (oaep)
+        {
+            if (isBase64)
+                return Detail::asymmetric<true, CryptoPP::RSAES_OAEP_SHA_Encryptor, CryptoPP::Base64Decoder>(data, Detail::rsaBase64KeyNormalize(publicKey));
+            else
+                return Detail::asymmetric<true, CryptoPP::RSAES_OAEP_SHA_Encryptor, CryptoPP::HexDecoder, CryptoPP::HexEncoder>(data, publicKey);
+        }
+        else
+        {
+            if (isBase64)
+                return Detail::asymmetric<true, CryptoPP::RSAES_PKCS1v15_Encryptor, CryptoPP::Base64Decoder>(data, Detail::rsaBase64KeyNormalize(publicKey));
+            else
+                return Detail::asymmetric<true, CryptoPP::RSAES_PKCS1v15_Encryptor, CryptoPP::HexDecoder, CryptoPP::HexEncoder>(data, publicKey);
+        }
+    }
+
+    std::string rsaDecrypt(const std::string &privateKey, const std::string &data, bool oaep)
+    {
+        bool isBase64 = Detail::isBase64String(privateKey);
+
+        if (oaep)
+        {
+            if (isBase64)
+                return Detail::asymmetric<false, CryptoPP::RSAES_OAEP_SHA_Decryptor, CryptoPP::Base64Decoder>(data, Detail::rsaBase64KeyNormalize(privateKey));
+            else
+                return Detail::asymmetric<false, CryptoPP::RSAES_OAEP_SHA_Decryptor, CryptoPP::HexDecoder>(data, privateKey);
+        }
+        else
+        {
+            if (isBase64)
+                return Detail::asymmetric<false, CryptoPP::RSAES_PKCS1v15_Decryptor, CryptoPP::Base64Decoder>(data, Detail::rsaBase64KeyNormalize(privateKey));
+            else
+                return Detail::asymmetric<false, CryptoPP::RSAES_PKCS1v15_Decryptor, CryptoPP::HexDecoder>(data, privateKey);
+        }
     }
 }
